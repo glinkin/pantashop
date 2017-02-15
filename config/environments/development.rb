9a7d5b94a5cacf026bafd2b471e5f098727c2b6b
glinkin <djbm@mail.ru>
2017-02-15 03:49:30 +0300

    Fix mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,12 +27,22 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.delivery_method = :sendmail
+  #config.action_mailer.delivery_method = :sendmail
   # Defaults to:
   # config.action_mailer.sendmail_settings = {
   #   location: '/usr/sbin/sendmail',
   #   arguments: '-i -t'
   # }
+  #config.action_mailer.perform_deliveries = true
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+
+  config.action_mailer.delivery_method = :sendmail
+
+  config.action_mailer.sendmail_settings = {
+      location: '/usr/sbin/sendmail',
+      arguments: '-i -t'
+  }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'sale@pantashop.ru'}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,11 +56,11 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "pantashop_#{Rails.env}"
   config.action_mailer.delivery_method = :sendmail
-  # Defaults to:
-  # config.action_mailer.sendmail_settings = {
-  #   location: '/usr/sbin/sendmail',
-  #   arguments: '-i -t'
-  # }
+
+  config.action_mailer.sendmail_settings = {
+      location: '/usr/sbin/sendmail',
+      arguments: '-i -t'
+  }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'sale@pantashop.ru'}
